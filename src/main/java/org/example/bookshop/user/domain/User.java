871524f24fd8118c394132cbcc159784ac584625
isobin <lee040319ww@naver.com>
2025-09-27 00:21:51 +0900

    UserDomain 세팅함

---
+++ src/main/java/org/example/bookshop/user/domain/User.java
@@ -0,0 +1,22 @@
+package org.example.bookshop.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+public class User {
+    @Id
+    @Column(name = "user_id")
+    private Long id;
+    private String name;
+    private String identification;
+    private String password;
+    private String email;
+
+
+}
